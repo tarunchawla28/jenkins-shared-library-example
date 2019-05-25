825393562fd474d4ff6afb4dfa729ef7b39cbe99
Tarun Chawla <37270001+tarunchawla28@users.noreply.github.com>
2019-05-25 20:10:38 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(String mvnHome){
-withEnv(["MVN_HOME=$mvnHome"]){
-    $MVN_HOME/bin/mvn clean package
+    withEnv(["${MVN_HOME}=${mvnHome}"]){
+        ${MVN_HOME}/bin/mvn clean package
    }
 }
