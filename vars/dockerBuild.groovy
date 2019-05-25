d52a5fbd0095f76f2138310bc80a535c869a5beb
Tarun Chawla <37270001+tarunchawla28@users.noreply.github.com>
2019-05-25 20:57:58 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,11 @@
 def call(String imageName){
   sh 'pwd'
   echo "${imageName}"
-  sh 'sudo docker build -t "${imageName}" .'
+  sh 'sudo docker build -t ${imageName}'
 }
-
+#!/usr/bin/env groovy
+def call(String MVN_HOME){
+        withEnv(["mvnHome=${MVN_HOME}"]){
+        sh '${mvnHome}/bin/mvn clean package'
+        }
+}
